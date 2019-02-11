bc1f2e04dab89869206d0095e149fc023c41191c
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-11 17:46:58 -0600

    created: NamesController Class

--- src/main/java/com/dkm/javacountries/NamesController.java
+++ src/main/java/com/dkm/javacountries/NamesController.java
@@ -3,6 +3,7 @@
 package com.dkm.javacountries;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,6 +17,36 @@ public class NamesController
     {
         JavaCountriesApplication.countriesList.countryList.sort((o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
         return JavaCountriesApplication.countriesList.countryList;
+    }
+
+    @RequestMapping("/begin")
+    public ArrayList<Country> filterCountryByLetter(@RequestParam(value="letter") char letter)
+    {
+        ArrayList<Country> givenLetter = new ArrayList<>();
+
+        for (Country e : JavaCountriesApplication.countriesList.countryList)
+        {
+            if (Character.toLowerCase(e.getName().charAt(0)) == Character.toLowerCase(letter))
+            {
+                givenLetter.add(e);
+            }
+        }
+        return givenLetter;
+    }
+
+    @RequestMapping("/size")
+    public ArrayList<Country> filterCoutryByLength(@RequestParam(value="letters") int length)
+    {
+        ArrayList<Country> givenLength = new ArrayList<>();
+
+        for (Country e : JavaCountriesApplication.countriesList.countryList)
+        {
+            if (e.getName().length() >= length)
+            {
+                givenLength.add(e);
+            }
+        }
+        return givenLength;
     }
 
 }
