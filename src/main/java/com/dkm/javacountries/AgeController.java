5c71728076b469d66070af575c66600540a70736
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-12 01:07:40 -0600

    created: AgeController Class
    did it in a dynamic way

---
+++ src/main/java/com/dkm/javacountries/AgeController.java
@@ -0,0 +1,56 @@
+package com.dkm.javacountries;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/age")
+public class AgeController
+{
+    @RequestMapping("/age")
+    public ArrayList<Country> overMedianAge(@RequestParam(value="age") int age)
+    {
+        ArrayList<Country> countriesOverMedian = new ArrayList<>();
+
+        for (Country e : JavaCountriesApplication.countriesList.countryList)
+        {
+            if (e.getMedianAge() > age)
+            {
+                countriesOverMedian.add(e);
+            }
+        }
+
+        return countriesOverMedian;
+    }
+
+    public Country YoungestAndOldest(String YO)
+    {
+        ArrayList<Country> sortedByAge = JavaCountriesApplication.countriesList.countryList;
+        sortedByAge.sort((a, b) -> a.getMedianAge() - b.getMedianAge());
+
+        if (YO.equalsIgnoreCase("youngest"))
+        {
+            return sortedByAge.get(0);
+        }
+        else
+        {
+            return sortedByAge.get(sortedByAge.size()-1);
+        }
+    }
+
+    @RequestMapping("/min")
+    public Country minAge()
+    {
+        return YoungestAndOldest("youngest");
+    }
+
+    @RequestMapping("/max")
+    public  Country maxAge()
+    {
+        return YoungestAndOldest("oldest");
+    }
+
+}
